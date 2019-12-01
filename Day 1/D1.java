274723b99b76d1526abef2db5b4842bcc0c6e7be
Hak <hk14989@bristol.ac.uk>
2019-12-01 19:22:23 +0000

    Updated D1.java with part 2's answer

--- Day 1/D1.java
+++ Day 1/D1.java
@@ -3,28 +3,55 @@ import java.io.FileReader;
 
 public class D1 {
     public static void main(String[] args) {
-        int cases[] = new int[100];
 
-        try (BufferedReader br = new BufferedReader(new FileReader("D1.input"))) {
+    	int cases[] = new int[100];
+    	int answers[] = new int[100];
+
+    	readFile(cases, "D1.input");
+
+    	System.out.println("Part 1 answer is: " + part1(cases));
+    	System.out.println("Part 2 answer is: " + part2(cases));
+        
+    }
+
+    public static int part1(int[] cases){
+		
+		int total = 0;
+
+		for (int i = 0; i < 100; i++){
+			total += calcFuel(cases[i]);
+		}
+		return total;
+    }
+
+    public static int part2(int[] cases){
+    	int total = 0;
+
+    	for (int i = 0; i < 100; i++){
+    		int a = cases[i];
+
+    		while (calcFuel(a) > 0){
+    			int b = calcFuel(a);
+				total += b;
+				a = b;
+			}
+		}
+
+    	return total;
+    }
+
+    public static int calcFuel(int a){
+    	// Java always rounds down in the case of integer division, so we do not need to implement this
+    	return (a/3) - 2;
+    }
+
+    public static void readFile(int[] cases, String filename){
+    	try (BufferedReader br = new BufferedReader(new FileReader("D1.input"))) {
 	    	for (int i = 0; i < 100; i++) {
 	       		cases[i] = Integer.parseInt(br.readLine());
     		}
 		} catch (Exception e){
 			System.out.println(e);
 		}
-
-		int answers[] = new int[100];
-		int total = 0;
-
-		for (int i = 0; i < 100; i++){
-			// Java always rounds down in the case of integer division, so we do not need to implement this
-			int a = cases[i]/3;
-
-			a -= 2;
-
-			total += a;
-		}
-
-		System.out.println(total);
     }
 }
